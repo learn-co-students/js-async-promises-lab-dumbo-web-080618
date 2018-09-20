701f714738fb24082bd00d7e8d16a9a1aafaeefc
skarimo <karimovj@gmail.com>
2018-09-20 11:36:59 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,51 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  let container = document.querySelector(".question-container")
+  container.innerHTML = question.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  return new Promise((resolve,reject) => {
+    setTimeout(function(){
+    }, time)
+  })
+}
+
+function removeQuestion(){
+  return new Promise((resolve,reject) => {
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion(question)
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  return x = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons() {
+  let x = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  x.forEach(function(func) {
+    func.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  console.log(btn)
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+
+//
